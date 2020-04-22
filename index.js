4224e873f241b8b8a1fdc9d72e4583d6326bce4b
syedm1 <31075630+syedm1@users.noreply.github.com>
2020-04-23 01:21:46 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+const randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+const randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+const image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+const randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
